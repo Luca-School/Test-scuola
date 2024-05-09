6bd90fa1c667cd3fa96104e2a509679b7ff80cf7
Luca-School <luca.notarantonio@allievi.bearzi.it>
2024-05-09 09:52:39 +0200

    Add files via upload

---
+++ Insertion-N°1.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+void insertionSortIterativo(int array[]){
+	for(int i = 1; i < )
+}
+
+printArray(int size, int array[size]){
+	for(int i = 0; i < size; i++){
+		printf("%d\t", array[i]);
+	}
+}
+
+int main(){
+	
+	int array[] = {1,4,2,6};
+	insertionSortIterativo(array[]);
+	return 0;
+}
